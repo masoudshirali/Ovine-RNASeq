c75c152151313aae5971bd7e890433e19222bea5
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2024-02-24 21:31:02 +0000

    Update extracting_IDs_from_gtfFile.sh

--- extracting_IDs_from_gtfFile.sh
+++ extracting_IDs_from_gtfFile.sh
@@ -8,7 +8,7 @@ zgrep 'GeneID' $gfffile \
 
 # remove GeneID from all the lines and add column names
 sed 's/GeneID://g' $gffpath/extracted_entrezids > $gffpath/entrezids 
-sed  -i '1i GeneID\tGeneName' $gffpath/entrezids
+sed  -i '1i GeneID\tGeneName' $gffpath/entrezids # 35106 total
 
 # getting description
 grep 'GeneID' $gfffile \
@@ -23,7 +23,7 @@ rm extracted_entrezids_with_description
 rm extracted_entrezids
 
 # extract the gene ontology ids and gene ids from the .gaf file (downloaded from NCBI)
-awk -F '\t| |"' -v OFS="\t" '{print $2,$5}' $gffpath/GCF_016772045.2-RS_2023_10_gene_ontology.gaf > $gffpath/entrezids_GOids
+awk -F '\t| |"' -v OFS="\t" '{print $2,$5}' $gffpath/GCF_016772045.2-RS_2023_10_gene_ontology.gaf > $gffpath/entrezids_GOids #93113 IDs only have GO Ids
 
-# combine the GOids and description file
-# awk 'NR==FNR {h[$1] = $2; next} {print $1,$2,$3,h[$1]}' $gffpath/entrezids_with_description $gffpath/geneids_GOIds > $gffpath/entrezids_with_GOids_and_description
+# combine the GOids and entrezIds file, because the GO ID file does not have all the entrezIDs of rambouillet genome
+awk 'NR==FNR {h[$1] = $2; next} {print $1,$2,$3,h[$1]}' $gffpath/entrezids_GOids $gffpath/entrezids > $gffpath/Full_entrezids_with_GOids
