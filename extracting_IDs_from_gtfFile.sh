e537d60249f69c4dd2d00db66e2202d6e4133e4e
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2024-02-24 21:34:56 +0000

    Update extracting_IDs_from_gtfFile.sh

--- extracting_IDs_from_gtfFile.sh
+++ extracting_IDs_from_gtfFile.sh
@@ -26,4 +26,4 @@ rm extracted_entrezids
 awk -F '\t| |"' -v OFS="\t" '{print $2,$5}' $gffpath/GCF_016772045.2-RS_2023_10_gene_ontology.gaf > $gffpath/entrezids_GOids #93113 IDs only have GO Ids
 
 # combine the GOids and entrezIds file, because the GO ID file does not have all the entrezIDs of rambouillet genome
-awk 'NR==FNR {h[$1] = $2; next} {print $1,$2,$3,h[$1]}' $gffpath/entrezids_GOids $gffpath/entrezids > $gffpath/Full_entrezids_with_GOids
+awk 'NR==FNR {h[$1] = $2; next} {print $1"\t"$2"\t"h[$1]}' $gffpath/entrezids_GOids $gffpath/entrezids > $gffpath/Full_entrezids_with_GOids
