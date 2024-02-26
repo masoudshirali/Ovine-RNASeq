4cdb047af6c8d06e24ffcce642767ec59d28b010
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2024-02-26 15:23:44 +0000

    Update Deseq2_on_wgcna_important_modules.R

--- Deseq2_on_wgcna_important_modules.R
+++ Deseq2_on_wgcna_important_modules.R
@@ -43,7 +43,7 @@ deseq2Data <- DESeq(deseq2Data)
 # RELAXED - with reads not removed
 
 pval = 0.1
-lfc = 0.584
+lfc = 0
 results = resultsNames(deseq2Data)
 upresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"upDEGs"))
 downresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"downDEGs"))
@@ -56,7 +56,7 @@ for(i in 1:length(results)){
   upDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange > lfc))))
   downDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange < -lfc))))
   resSig = subset(resorder, padj < pval & log2FoldChange > lfc | padj < pval & log2FoldChange < -lfc)
-  write.csv(resSig , file=paste0("7.wgcna/",results[i],".0.1P.0.584LFC.updownDEGs_STRINGENT.csv"), row.names = T)
+  write.csv(resSig , file=paste0("7.wgcna/",results[i],".0.1P.0LFC.updownDEGs_STRINGENT.csv"), row.names = T)
   upresultstable[results[i],"upDEGs"] = upDEGs
   downresultstable[results[i],"downDEGs"] = downDEGs 
 }
@@ -80,3 +80,109 @@ for(i in 1:length(results)){
   downresultstable[results[i],"downDEGs"] = downDEGs 
 }
 
+# Enrichment Analysis of the genes
+# Taking the resSig variable from the above deseq2 (one with p<0.1 and lfc=0 stringent approach)
+resSig$GeneID = rownames(resSig)
+YgPvrEntrez <- AnnotationDbi::select(Oaries, keys =  rownames(resSig),
+  columns = c('ENTREZID','GENENAME'), keytype = 'SYMBOL') # Oaries is the annotation db created from the geneset_enrichment_analysis.R code
+
+# Now replace the NA values in entrezid column with the values from first column. This is done because many values were not converted to ENTREZ IDs but already has it from GTF file. We will retain those.
+YgPvrEntrez$ENTREZID <- ifelse(is.na(YgPvrEntrez$ENTREZID), YgPvrEntrez$SYMBOL, YgPvrEntrez$ENTREZID)
+colnames(YgPvrEntrez) = c("GeneID", "ENTREZID")
+YgPvr_with_entrez = merge(as.data.frame(resSig), YgPvrEntrez, by = "GeneID") #All entrezIDs have been retrieved for the 36 genes with p<0.1 and lfc=0 threshold
+
+# Extract entrezIDs for the res
+res_allgenes<-res
+res_allgenes$GeneID<-rownames(res)
+
+# removing "LOC" info
+res_allgenes$GeneID <- stringr::str_remove(res_allgenes$GeneID, "LOC")
+rownames(res_allgenes) = stringr::str_remove(rownames(res_allgenes), "LOC")
+
+res_allgenesEntrez <- AnnotationDbi::select(Oaries, keys =  res_allgenes$GeneID,
+  columns = c('ENTREZID','GENENAME'), keytype = 'SYMBOL')
+
+# Now replace the NA values in entrezid column with the values from first column. This is done because many values were not converted to ENTREZ IDs but already has it from GTF file. We will retain those.
+res_allgenesEntrez$ENTREZID <- ifelse(is.na(res_allgenesEntrez$ENTREZID), res_allgenesEntrez$SYMBOL, res_allgenesEntrez$ENTREZID)
+colnames(res_allgenesEntrez) = c("GeneID", "ENTREZID")
+res_allgenes_with_entrez = merge(as.data.frame(res_allgenes), res_allgenesEntrez, by = "GeneID") #All entrezIDs have been retrieved for the DEGs
+
+#####################################################
+# GO AND KEGG ENRICHMENTS
+#####################################################
+
+# In order to asses functional enrichment, both DE gene list and gene universe must be annotated in Entrez IDs:
+
+res_universe<-res_allgenes_with_entrez$ENTREZID
+# omit any NA values 
+res_universe<-na.omit(res_universe)
+# sort the list in decreasing order (required for clusterProfiler)
+res_universe = sort(res_universe, decreasing = TRUE)
+res_sigGenes<-YgPvr_with_entrez$ENTREZID
+res_sigGenes = sort(res_sigGenes, decreasing = TRUE)
+
+ans.go <- enrichGO(gene = res_sigGenes, ont = "ALL",
+                   OrgDb = Oaries,
+                   universe = res_universe,
+                   readable=TRUE,
+                   pvalueCutoff = 0.05)
+tab.go <- as.data.frame(ans.go)
+write.csv(tab.go,"8.geneset.enrichments/WGCNA_Yg_Pvr_GO_enrichments.csv")
+
+ans.kegg <- enrichKEGG(gene = res_sigGenes,
+                       organism = 'oas',
+                       universe = res_universe,
+                       pvalueCutoff = 0.05)
+tab.kegg <- as.data.frame(ans.kegg)
+write.csv(tab.kegg,"8.geneset.enrichments/WGCNA_Yg_Pvr_KEGG_enrichments.csv")
+
+#####################################################
+# VISUALIZATIONS OF GO AND KEGG ENRICHMENTS
+#####################################################
+
+pdf("8.geneset.enrichments/WGCNA_Yg_Pvr_GO-Barplot.pdf", height=14)
+barplot(ans.go, showCategory=20)
+dev.off()
+
+pdf("8.geneset.enrichments/WGCNA_Yg_Pvr_upsetplot_GO.pdf")
+upsetplot(ans.go, showCategory=100)
+dev.off()
+
+pdf("8.geneset.enrichments/WGCNA_Yg_Pvr_upsetplot_kegg.pdf")
+upsetplot(ans.kegg, showCategory=30)
+dev.off()
+
+pdf("8.geneset.enrichments/WGCNA_Yg_Pvr_emapplot_kegg.pdf", width=12)
+emapplot(pairwise_termsim(ans.kegg))
+dev.off()
+
+# In order to consider the potentially biological complexities in which a gene may belong to multiple annotation categories,  cnetplot function will extract the complex association between genes and pathways.
+pdf("8.geneset.enrichments/WGCNA_Yg_Pvr_cnetplot_kegg.pdf", width=12)
+cnetplot(ans.kegg, categorySize="pvalue", foldChange=res_sigGenes)
+dev.off()
+
+#####################################################
+# PATHWAY ANALYSIS - RETRIEVING PATHWAY IMAGES
+#####################################################
+
+#preparing tables for pathway analysis
+de=data.frame(YgPvr_with_entrez$ENTREZID,YgPvr_with_entrez$log2FoldChange)
+dl=data.frame(YgPvr_with_entrez$GeneID,YgPvr_with_entrez$log2FoldChange)
+head(de)
+head(dl)
+
+geneList = de[,2]
+names(geneList) = as.character(de[,1])
+geneList = sort(geneList, decreasing = TRUE)
+head(geneList)#geneList has entrezids and FC values
+gene <- names(geneList)
+
+# Retrieveing the pathway images
+pathwayids=ans.kegg$ID   #make a vector of Pathway ids
+keggspecies="oas"
+
+x <- pathview(gene.data  = geneList,
+              pathway.id = pathwayids,
+              species    = keggspecies,
+              gene.idtype = "KEGG",
+              limit      = list(gene=max(abs(geneList)), cpd=1))
