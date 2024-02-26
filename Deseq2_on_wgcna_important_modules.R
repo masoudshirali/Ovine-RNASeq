419236fef42f43f6a6d04f4d0c3715dd81ba518e
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2024-02-26 14:21:41 +0000

    Update Deseq2_on_wgcna_important_modules.R

--- Deseq2_on_wgcna_important_modules.R
+++ Deseq2_on_wgcna_important_modules.R
@@ -8,6 +8,9 @@ ygLfcRaw <- countData[rownames(countData) %in% yellowgreen$Genes,]
 pvrLfcRaw <- countData[rownames(countData) %in% palevioletred3$Genes,]
 yg_pvr_Raw = rbind(ygLfcRaw,pvrLfcRaw)
 
+# removing "LOC" info
+rownames(yg_pvr_Raw) = stringr::str_remove(rownames(yg_pvr_Raw), "LOC")
+
 # Read the metadata file
 metaData <-read.csv("metadata_with_methaneinfoadded_metadata.csv",sep=",",header=T)
 #metaData1 = metaData[!grepl('Low|Medium', metaData$ID),]# To remove any rows with Low or medium in the ID column. (Only for test case)
@@ -25,19 +28,21 @@ all(colnames(yg_pvr_Raw) == rownames(metaData))
 
 deseq2Data <- DESeqDataSetFromMatrix(countData=yg_pvr_Raw, colData=metaData, design= ~CH4production)
 
-# if you have many samples, consider doing this filtering step
-smallestGroupSize = 22 #total number of samples
-keep <- rowSums(counts(deseq2Data) >= 10) >= smallestGroupSize
-deseq2Data <- deseq2Data[keep,] #114 remaining
+# Stringent approach where we keep only rows that have at least 10 reads total
+keep <- rowSums(counts(deseq2Data)) >= 10
+deseq2Data <- deseq2Data[keep,] #240 remaining
 
 deseq2Data <- DESeq(deseq2Data)
 
 
 #loop through results and extract significant DEGs for each model term
 # speify the cut-offs for pval and lfc in the below variables.
-# make sure to change the filenames with the cutoff values before saving the deg file (Line 89)
+# make sure to change the filenames with the cutoff values before saving the deg file (Line 60)
 
-pval = 0.05
+# STRINGENT - with reads removed in steps 32 to 34
+# RELAXED - with reads not removed
+
+pval = 0.1
 lfc = 0.584
 results = resultsNames(deseq2Data)
 upresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"upDEGs"))
@@ -51,10 +56,27 @@ for(i in 1:length(results)){
   upDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange > lfc))))
   downDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange < -lfc))))
   resSig = subset(resorder, padj < pval & log2FoldChange > lfc | padj < pval & log2FoldChange < -lfc)
-  write.csv(resSig , file=paste0("7.wgcna/",results[i],".0.05P.0.584LFC.updownDEGs_STRINGENT.csv"), row.names = T)
+  write.csv(resSig , file=paste0("7.wgcna/",results[i],".0.1P.0.584LFC.updownDEGs_STRINGENT.csv"), row.names = T)
   upresultstable[results[i],"upDEGs"] = upDEGs
   downresultstable[results[i],"downDEGs"] = downDEGs 
 }
 
+pval = 0.1
+lfc = 0.584
+results = resultsNames(deseq2Data)
+upresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"upDEGs"))
+downresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"downDEGs"))
 
+for(i in 1:length(results)){
+
+  res = results(deseq2Data, 
+                name = results[i])
+  resorder <- res[order(res$padj),]
+  upDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange > lfc))))
+  downDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange < -lfc))))
+  resSig = subset(resorder, padj < pval & log2FoldChange > lfc | padj < pval & log2FoldChange < -lfc)
+  write.csv(resSig , file=paste0("7.wgcna/",results[i],".0.1P.0.584LFC.updownDEGs_RELAXED.csv"), row.names = T)
+  upresultstable[results[i],"upDEGs"] = upDEGs
+  downresultstable[results[i],"downDEGs"] = downDEGs 
+}
 
